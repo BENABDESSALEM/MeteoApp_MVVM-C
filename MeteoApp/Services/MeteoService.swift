71d880c0d12ab5736dd194769723c381eccd5a89
Wajih Benabdessalem <benabdessalemwajih1212@gmail.com>
2023-06-22 13:38:56 +0200

    Update MeteoService class

--- MeteoApp/Services/MeteoService.swift
+++ MeteoApp/Services/MeteoService.swift
@@ -12,12 +12,11 @@ public class MeteoService {
     
     let meteoService = APIManager(key: API_Key,apiUrl: API_BASE_URL,version: API_Version)
     
-    public func getCityByName(name: String,limit:Int,completion: @escaping (ApiResult<Weather>) -> Void) {
+    public func getCityByName(name: String,completion: @escaping (ApiResult<Weather>) -> Void) {
         meteoService.send(to: "weather", with: ["q": name], completion: completion)
     }
     
     public func getWeatherForLocation(latitude: String, longitude: String, completion: @escaping (ApiResult<Weather>) -> Void) {
         meteoService.send(to: "weather", with: ["lat": latitude, "lon": longitude], completion: completion)
     }
-
 }
