a86a816d9a5c8dc2b55d8e3af083466a41c93905
Wajih Benabdessalem <benabdessalemwajih1212@gmail.com>
2023-06-22 13:34:01 +0200

    Update city cellviewModel

--- MeteoApp/MVVM/ViewModels/CityCellViewModel.swift
+++ MeteoApp/MVVM/ViewModels/CityCellViewModel.swift
@@ -7,17 +7,19 @@
 
 import Foundation
 
-final class CityViewModel {
-    
-}
-
 final class CityCellViewModel {
     // MARK: - Constants.
+    let temperature: Bindable<String?> = Bindable("")
+    let tempMinMax: Bindable<String?> = Bindable("")
     let cityName: Bindable<String?> = Bindable("")
+    let status: Bindable<String?> = Bindable("")
     let pictoImage: Bindable<String?> = Bindable("")
     
     init(city:CityWeather) {
+        temperature.value = city.temp
+        tempMinMax.value = city.tempMinMax
         cityName.value = city.city
+        status.value = city.status
         pictoImage.value = city.image
     }
 }
