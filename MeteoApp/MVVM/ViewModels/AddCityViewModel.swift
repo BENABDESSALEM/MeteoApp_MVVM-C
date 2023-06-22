32e1b0e457909f904dcd64b0ef7c001b57178d1b
Wajih Benabdessalem <benabdessalemwajih1212@gmail.com>
2023-06-22 13:30:58 +0200

    Update AddCityViewModel

--- MeteoApp/MVVM/ViewModels/AddCityViewModel.swift
+++ MeteoApp/MVVM/ViewModels/AddCityViewModel.swift
@@ -10,10 +10,11 @@ import MeteoAPI
 
 class AddCityViewModel {
     // MARK:- Constants.
-    let results: Bindable<[Weather]> = Bindable([])
+    let results: Bindable<[CityWeather]> = Bindable([])
     let error: Bindable<String?> = Bindable(nil)
     let isButtonEnabled: Bindable<Bool> = Bindable(false)
     let isLoadingEnabled: Bindable<Bool> = Bindable(false)
+    let kKelvinZeroInCelsius = 273.15
     let meteoApi: MeteoService!
     // MARK:- Proprety observer.
     var searchText: String? = "" {
@@ -26,30 +27,44 @@ class AddCityViewModel {
         self.meteoApi = apiService
     }
     // MARK: SearchAction
-    func searchCity() {
+    func getCityWeather() {
         guard let text = searchText else { return }
-        let search = text.addingPercentEncoding(
+        let searchedCity = text.addingPercentEncoding(
             withAllowedCharacters: .urlHostAllowed
         ) ?? ""
         self.isLoadingEnabled.value = true
-        meteoApi.getCityByName(name: search, limit: 5) { result in
+        meteoApi.getCityByName(name: searchedCity) { result in
             self.isLoadingEnabled.value = false
             switch result {
             case .success(let weather):
-                self.results.value = [weather]
+                self.setupList(city: searchedCity, weather:weather)
             case .error(_):
                 self.error.value = "*** ERROR ***"
             }
         }
     }
-    
-    func getSearchResultVM(at index: Int) -> SearchResultViewModel {
-        let item = results.value[index]
-        return SearchResultViewModel(searchResult: item)
-    }
-    
+    /// Check the minimuim number of characters to start the search.
     func isCharactersReached() -> Bool {
         guard let searchText = searchText else { return false }
         return searchText.count >= 3
+    }
+}
+
+extension AddCityViewModel {
+    /// Setup the displayed list of cities.
+    func setupList(city:String,weather:Weather) {
+        let temp = "\(self.toCelsius(kelvin: (weather.main?.temp) ?? 15))"
+        let temp_min = "\(self.toCelsius(kelvin: (weather.main?.tempMin) ?? 15))"
+        let temp_max = "\(self.toCelsius(kelvin: (weather.main?.tempMax) ?? 15))"
+        let name = (weather.name ?? "") + ", " + (weather.sys?.country ?? "")
+        let tempMinMax = "H\(temp_max)°, L\(temp_min)°"
+        let status = weather.weather?.first?.description
+        let image = weather.weather?.first?.icon
+        let cityWeather = CityWeather(temp: temp, tempMinMax: tempMinMax, city: name, status: status, image: image)
+        self.results.value.append(cityWeather)
+    }
+    /// Converting the tempereture value to Celsius.
+    func toCelsius(kelvin: Double) -> Int {
+        return Int(kelvin - kKelvinZeroInCelsius)
     }
 }
