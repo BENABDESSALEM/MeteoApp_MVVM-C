95a33b405136c6d1d320287b0487421375d2634e
Wajih Benabdessalem <benabdessalemwajih1212@gmail.com>
2023-06-24 22:19:13 +0200

    Add Details view

--- MeteoApp/MVVM-C/Views/DetailsCityWeather/DetailsViewController.swift
+++ MeteoApp/MVVM-C/Views/DetailsCityWeather/DetailsViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 final class DetailsViewController: UIViewController {
 
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var averageTempLabel: UILabel!
+    @IBOutlet weak var weatherImgView: UIImageView!
+    @IBOutlet weak var windSpeedLabel: UILabel!
+    @IBOutlet weak var weeklyContainerView: UIView!
+    @IBOutlet weak var weatherCollectionView: UICollectionView!
+    
     // MARK: - Variables.
     var viewModel: DetailsViewModel!
     weak var coordinator: AppCoordinator?
@@ -29,6 +38,10 @@ final class DetailsViewController: UIViewController {
         super.viewDidLayoutSubviews()
         gradientLayer.frame = view.bounds
     }
+    
+    @IBAction func backButtonTapped(_ sender: Any) {
+        coordinator?.goBack()
+    }
 }
 
 // MARK: - Initialisation & setup methods.
@@ -38,6 +51,15 @@ extension DetailsViewController {
      Setup Views.
      */
     func setupViews() {
+        weatherCollectionView.delegate = self
+        weatherCollectionView.dataSource = self
+        weatherCollectionView.registerCell(type: WeekCollectionViewCell.self)
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 10
+        layout.minimumInteritemSpacing = 10
+        weatherCollectionView.collectionViewLayout = layout
+        weeklyContainerView.setCornerRadius(radius: 25)
         view.setBackgroundGradient(gradientLayer:gradientLayer,colors: appColors, isVertical: true)
     }
     
@@ -46,5 +68,34 @@ extension DetailsViewController {
      */
     func setupBindings() {
 
+    }
+}
+
+// MARK: - UICollectionViewDataSource.
+
+extension DetailsViewController:UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 7
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell:WeekCollectionViewCell = collectionView.dequeueCell(for: indexPath)
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegate.
+
+extension DetailsViewController:UICollectionViewDelegate {
+
+}
+
+// MARK: UICollectionViewDelegateFlowLayout.
+
+extension DetailsViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let padding: CGFloat = 1.0
+        let numberOfItemsPerRow: CGFloat = 5.0
+        let width = (collectionView.frame.width-padding)/numberOfItemsPerRow
+        return CGSize(width: width, height: 128)
     }
 }
--- MeteoApp/MVVM-C/Views/WeekCell/WeekCollectionViewCell.swift
+++ MeteoApp/MVVM-C/Views/WeekCell/WeekCollectionViewCell.swift
@@ -8,10 +8,37 @@
 import UIKit
 
 class WeekCollectionViewCell: UICollectionViewCell {
-
+    
+    // MARK: Outlets.
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var weatherImgView: UIImageView!
+    @IBOutlet weak var tempLabel: UILabel!
+    
+    // MARK: Variable.
+    var gradientLayer = CAGradientLayer()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        setupViews()
     }
 
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = contentView.bounds
+    }
+    
+    func setupWith(viewModel:WeekCellViewModel) {
+
+    }
+    
+    func setupBindings() {
+
+    }
+    
+    func setupViews() {
+        containerView.setBackgroundGradient(gradientLayer: gradientLayer, colors: buttonColors, isVertical: true)
+        containerView.layer.cornerRadius = 25
+        containerView.layer.masksToBounds = true
+    }
 }
