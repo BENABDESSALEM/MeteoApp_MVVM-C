86408c66b59ffb9718f84569a40bafa9e349187b
Wajih Benabdessalem <benabdessalemwajih1212@gmail.com>
2023-06-26 04:15:25 +0200

    Add internet monitor class

---
+++ MeteoApp/Helper/NetworkMonitor.swift
@@ -0,0 +1,39 @@
+//
+//  NetworkMonitor.swift
+//  MeteoApp
+//
+//  Created by Wajih Benabdessalem on 6/26/23.
+//
+
+import Network
+
+@available(iOS 12.0, *)
+class NetworkMonitor {
+    static let shared = NetworkMonitor()
+
+    let monitor = NWPathMonitor()
+    private var status: NWPath.Status = .requiresConnection
+    var isReachable: Bool { status == .satisfied }
+    var isReachableOnCellular: Bool = true
+
+    func startMonitoring() {
+        monitor.pathUpdateHandler = { [weak self] path in
+            self?.status = path.status
+            self?.isReachableOnCellular = path.isExpensive
+
+            if path.status == .satisfied {
+                print("We're connected!")
+            } else {
+                print("No connection.")
+            }
+            print(path.isExpensive)
+        }
+
+        let queue = DispatchQueue(label: "NetworkMonitor")
+        monitor.start(queue: queue)
+    }
+
+    func stopMonitoring() {
+        monitor.cancel()
+    }
+}
